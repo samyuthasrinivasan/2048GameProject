1120f8009e5532dda3c3a0c6a0b7ce25a5d7db25
Samyutha Srinivasan <65869364+samyuthasrinivasan@users.noreply.github.com>
2023-08-17 18:44:08 -0400

    added left/right methods

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import random
 from pynput import keyboard
 
-# need to fix this error
-
-# [2, 2, 2, 2]
-# [8, 4, 4, 4]
-# [4, 2, 2, 2]
-# [0, 0, 0, 2]
-#
-# [0, 2, 0, 0]
-# [2, 2, 2, 0]
-# [8, 4, 4, 2]
-# [4, 2, 2, 8]
-
-# jumps a step by adding the 4 from the added 2s to the nearby 4
-# check if its a sum, if not add it, if it is leave it be
 
 def print_matrix():
     for j in range(4):
@@ -22,7 +8,7 @@ def print_matrix():
     print("\n")
 
 
-def add_two( ):
+def add_two():
     x = random.randint(0, 3)
     y = random.randint(0, 3)
     while matrix[x][y] != 0:
@@ -37,6 +23,22 @@ def set_below(row, col):
         below = matrix[row + 1][col]
         row += 1
     return row
+
+
+def set_left(row, col):
+    left = 0
+    while left == 0 and col > 0:
+        left = matrix[row][col - 1]
+        col -= 1
+    return col
+
+
+def set_right(row, col):
+    right = 0
+    while right == 0 and col < 3:
+        right = matrix[row][col + 1]
+        col += 1
+    return col
 
 
 def set_above(row, col):
@@ -169,8 +171,126 @@ def move_up():
 def move_right(row, col):
     sum = matrix[row][col]
 
-def move_left(row, col):
-    sum = matrix[row][col]
+
+def left(row, col):
+    point = matrix[row][col]
+    sum = point
+    right_side = 0
+    left_side = 0
+
+    if col == 0:
+        selected_right = set_right(row, col)
+        right_side = matrix[row][selected_right]
+        left_side = 1
+
+    if col == 3:
+        selected_left = set_left(row, col)
+        left_side = matrix[row][selected_left]
+        right_side = 1
+
+    if col > 0 and col < 3:
+        selected_left = set_left(row, col)
+        selected_right = set_right(row, col)
+        left_side = matrix[row][selected_left]
+        right_side = matrix[row][selected_right]
+
+    if left_side == point:
+        sum += left_side
+        matrix[row][col] = 0
+        matrix[selected_left][col] = 0
+
+    else:
+        if right_side == point:
+            sum += right_side
+            matrix[row][col] = 0
+            matrix[row][selected_right] = 0
+
+        else:
+            matrix[row][col] = 0
+
+    iteratorCol = col
+    pointCol = col
+    while matrix[row][iteratorCol] == 0 & iteratorCol > -1:
+        pointCol = iteratorCol
+        iteratorCol -= 1
+        if iteratorCol == -1:
+            break
+
+    matrix[row][pointCol] = sum
+
+
+def move_left():
+    for r in range(4) :
+        for c in range(4) :
+            if matrix[r][c] != 0:
+               row = r
+               col = c
+               left(row, col)
+
+    add_two()
+    print_matrix()
+
+
+def right(row, col):
+    point = matrix[row][col]
+    sum = point
+    right_side = 0
+    left_side = 0
+
+    if col == 0:
+        selected_right = set_right(row, col)
+        right_side = matrix[row][selected_right]
+        left_side = 1
+
+    if col == 3:
+        selected_left = set_left(row, col)
+        left_side = matrix[row][selected_left]
+        right_side = 1
+
+    if col > 0 and col < 3:
+        selected_left = set_left(row, col)
+        selected_right = set_right(row, col)
+        left_side = matrix[row][selected_left]
+        right_side = matrix[row][selected_right]
+
+    if right_side == point:
+        sum += right_side
+        matrix[row][col] = 0
+        matrix[row][selected_right] = 0
+
+    else:
+        if left_side == point:
+            sum += left_side
+            matrix[row][col] = 0
+            matrix[row][selected_left] = 0
+
+        else:
+            matrix[row][col] = 0
+
+
+    iteratorCol = col
+    pointCol = col
+    while matrix[row][iteratorCol] == 0 & iteratorCol < 4:
+        pointCol = iteratorCol
+        iteratorCol += 1
+        if iteratorCol == 4:
+            break
+
+    matrix[row][pointCol] = sum
+
+
+def move_right():
+
+    for r in range(4):
+        for c in range(4):
+            if matrix[3 - r][3 - c] != 0:
+                row = 3 - r
+                col = 3 - c
+                right(row, col)
+
+    add_two()
+    print_matrix()
+
 
 print("2048!")
 matrix = []
@@ -178,9 +298,11 @@ for i in range(4):
     matrix.append([0] * 4)
 
 x = random.randint(0, 3)
+
 y = random.randint(0, 3)
 matrix[y][x] = 2
 print_matrix()
+
 
 while True:
     def on_press(key):
@@ -191,16 +313,17 @@ while True:
             move_down()
             listener.stop()
         if key == keyboard.Key.left:
-            move_left(1, 1)
+            move_left()
             listener.stop()
         if key == keyboard.Key.right:
-            move_right(1, 1)
+            move_right()
             listener.stop()
         if key == keyboard.Key.esc:
             listener.stop()
 
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
+
 
 
 
